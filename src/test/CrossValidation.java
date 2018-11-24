d8396c3310a47ee8b040edec02f81e385c7a415e
CillianFn <cfennell.stflannans@gmail.com>
2018-11-24 16:52:19 +0000

    HashMap Split: - TODO: Pass by reference issue causing each value to be the same

--- src/test/CrossValidation.java
+++ src/test/CrossValidation.java
@@ -16,26 +16,35 @@ public class CrossValidation {
         this.instances = p.getInstances();
     }
 
-    private HashMap splitLists(int i){
+    private HashMap splitLists(int n){
 
-        //Shuffle data before splitting
-        Collections.shuffle(instances);
-
-        //Splitting instances into training and test datasets
-        int trainingUpper = (int) Math.floor(instances.size() * 0.66);
-        List<Instance> training = instances.subList(0, trainingUpper);
-        List<Instance> test = instances.subList(trainingUpper, instances.size() - 1);
-
-        //Populating maps
         HashMap<String, HashMap<Integer, List<Instance>>> parentMap = new HashMap<>();
         HashMap<Integer, List<Instance>> trainingMap = new HashMap<>();
         HashMap<Integer, List<Instance>> testMap = new HashMap<>();
 
-        trainingMap.put(i, training);
-        testMap.put(i, test);
+        for(int i = 1; i <= n; i++) {
+            Collections.shuffle(instances);
 
-        parentMap.put("training", trainingMap);
-        parentMap.put("test", testMap);
+            //Splitting instances into training and test datasets
+            int trainingUpper = (int) Math.floor(instances.size() * 0.66);
+            List<Instance> training = instances.subList(0, trainingUpper);
+            List<Instance> test = instances.subList(trainingUpper, instances.size() - 1);
+
+            //Populating maps
+
+
+            trainingMap.put(i, training);
+            testMap.put(i, test);
+
+            parentMap.put("training", trainingMap);
+            parentMap.put("test", testMap);
+        }
+
+        for(Map.Entry<String, HashMap<Integer,List<Instance>>> t :parentMap.entrySet()){
+            String key = t.getKey();
+            for (Map.Entry<Integer,List<Instance>> e : t.getValue().entrySet())
+                System.out.println("Dataset: " + key + " Iteration: " + e.getKey()+ " Instances: " +e.getValue());
+        }
 
         return parentMap;
     }
@@ -44,18 +53,17 @@ public class CrossValidation {
         return instances;
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         String fileName = "owls.csv";
-        String[] attributes = { "body-length real n", "wing-length real n", "body-width real n", "wing-width real n",
-                "type [Barn_Owl,Snowy_Owl,Long-Eared_Owl] target" };
+        String[] attributes = {"body-length real n", "wing-length real n", "body-width real n", "wing-width real n",
+                "type [Barn_Owl,Snowy_Owl,Long-Eared_Owl] target"};
         ProcessInput pi = new ProcessInput(attributes, fileName);
 
         CrossValidation cv = new CrossValidation(pi);
 
         //Printing HashMap
-        for(int i = 1; i <=10; i++){
-            System.out.println(Arrays.asList(cv.splitLists(i)));
-        }
+        //System.out.println(Arrays.asList(cv.splitLists(10)));
+        cv.splitLists(10);
     }
 
 }
